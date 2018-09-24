a58d9cf4a94280f7c7255daed601ceaae6a4e11a
warryr <heartsyrop@gmail.com>
2018-09-24 18:43:07 +0300

    I tried

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var temp = [];
+  
+  for (var i = 0; i < str.length; i++){
+    if (str[i] == '(' || str[i] == '[' || str[i] == '{'){
+      temp.push(str[i]);
+    } else if (str[i] == ')' || str[i] == ']' || str[i] == '}'){
+      if (temp.length == 0){
+            return false;
+          } else{
+            open = temp.pop();
+            if (!(open == '(' && str[i] == ')') && !(open == '[' && str[i] == ']') && !(open == '{' & str[i] == '}')){
+                return false;
+            }
+        }
+    }
+  }
+  if (temp.length > 0){
+    return false;
+  }
+  return true;
 }
