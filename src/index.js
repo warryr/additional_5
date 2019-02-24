5bd7c68d28217facc006616466b87b35178842b1
warryr <heartsyrop@gmail.com>
2019-02-24 15:23:09 +0300

    refactor: refactor index.js

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ module.exports = function check(str, bracketsConfig) {
     let pair = tryGetCloserPair(str[i]);
 
     if (pair != null){
-      if (stack.length == 0){
-        if (pair == str[i]){
+      if (stack.length === 0){
+        if (pair === str[i]){
           stack.push(str[i]);
         } else{
           return false;
@@ -14,8 +14,8 @@ module.exports = function check(str, bracketsConfig) {
       }
       else{
         const temp = stack.pop();
-        if (temp != pair){
-          if (pair == str[i]){
+        if (temp !== pair){
+          if (pair === str[i]){
             stack.push(temp, str[i]);
           } else{
             return false;
@@ -26,14 +26,14 @@ module.exports = function check(str, bracketsConfig) {
       stack.push(str[i]);
     }
   }
-  return stack.length == 0;
+  return stack.length === 0;
 
   function tryGetCloserPair(bracket){
     for (let i = 0, len = bracketsConfig.length; i < len; i++){
-      if (bracket == bracketsConfig[i][1]){
+      if (bracket === bracketsConfig[i][1]){
         return bracketsConfig[i][0];
       }
     }
     return null;
   }
-}
+};
